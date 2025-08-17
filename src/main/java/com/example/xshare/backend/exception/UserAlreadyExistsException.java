ad0f44c6988a5098dd7fa642d325c7775d5f4554
Jashanpreet Singh <jashanpreetsingh1096@gmail.com>
2025-08-18 01:24:42 +0530

    Code Cleanup

--- src/main/java/com/example/xshare/backend/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.xshare.backend.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
